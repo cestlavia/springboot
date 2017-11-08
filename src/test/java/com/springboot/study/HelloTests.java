6ef48f692bbeca7e782354772dcea6d0b0af53e8
wangmingliang <rszswang@126.com>
2017-11-08 20:04:00 +0800

    init Spring Boot Project

---
+++ src/test/java/com/springboot/study/HelloTests.java
@@ -0,0 +1,27 @@
+package com.springboot.study;
+
+import com.springboot.study.controller.HelloWorld;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloTests {
+    private MockMvc mvc;
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorld()).build();
+    }
+    public void getHello() throws  Exception {
+        /*mvc.perform(MockMvcRequestBuilders.get("/hello")
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOK())
+        .andExpect(content().string(equalTo("Hello World")));*/
+    }
+}
